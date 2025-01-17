9a7a6df21a5a7affe3b6a0e4882976385eb536f2
Savina Dzhenimova <inka0145@abv.bg>
2025-01-17 16:49:34 +0200

    Update Person.java
    Change name to Unknown

--- Person.java
+++ Person.java
@@ -15,7 +15,7 @@ public class Person {
 
     // Default Constructor
     public Person() {
-        this.name = "Savina";
+        this.name = "Unknown";
         this.age = 0;
         this.address = "Unknown";
     }
