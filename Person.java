8b43326d3b30c2caa9e5cb51c45f7d108d8d46fe
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-01-17 16:18:35 +0200

    Some changes by savvdzh

--- Person.java
+++ Person.java
@@ -4,6 +4,8 @@ public class Person {
     private int age;
     private String address;
 
+    // Some changes to create conflict
+
     // Constructor for Person class
     public Person(String name, int age, String address) {
         this.name = name;
